674cb37fab1d3e2d1416572a133558a0522850fa
saira00 <160352041+saira00@users.noreply.github.com>
2024-04-07 15:53:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// const randomNumber = Math.random();
+// console.log(randomNumber);
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+
+const answer = await inquirer.prompt([
+  { 
+    name: "UserGuessednumber",
+    type: "number",
+    message: "please guess a number between 1-5:",
+  },
+]);
+
+if(answer.UserGuessednumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+} else {
+    console.log("you guessed worng number");
+}
+    
